e4bcb6931b528088983c92b5ddd65b460f6aa4d0
oshcheholev <oshcheho@student.42vienna.com>
2026-04-09 15:56:52 +0200

    FEAT: mariadb config

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-service mariadb start
+set -e
 
-# Wait for MariaDB to be ready
-sleep 5
+chown -R mysql:mysql /var/lib/mysql /run/mysqld
+
+if [ ! -d /var/lib/mysql/mysql ]; then
+	mariadb-install-db --user=mysql --datadir=/var/lib/mysql --auth-root-authentication-method=normal --skip-test-db >/dev/null
+fi
+
+mysqld_safe --user=mysql --datadir=/var/lib/mysql --socket=/run/mysqld/mysqld.sock &
+server_pid=$!
+
+until mysqladmin ping -u root --silent >/dev/null 2>&1; do
+	sleep 1
+done
 
 mysql -u root <<EOF
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
@@ -16,8 +26,4 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
 
-# Stop service to restart in foreground
-mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
-
-# Run MariaDB in foreground (IMPORTANT for Docker)
-exec mysqld_safe
+wait ${server_pid}

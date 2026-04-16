3810af373b6300d459c1b5050297af3a297c5e3b
oshcheho <oshcheho@student.42vienna.com>
2026-04-16 15:34:29 +0200

    changed some paths

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -8,22 +8,22 @@ if [ ! -d /var/lib/mysql/mysql ]; then
     mariadb-install-db --user=mysql --datadir=/var/lib/mysql --auth-root-authentication-method=normal --skip-test-db >/dev/null
 fi
 
-mysqld_safe --user=mysql --datadir=/var/lib/mysql --socket=/run/mysqld/mysqld.sock --bind-address=0.0.0.0 &
-server_pid=$!
+# Create initialization SQL
+cat <<EOF > /tmp/init.sql
+USE mysql;
+FLUSH PRIVILEGES;
 
-until mysqladmin ping -u root -p"${MYSQL_ROOT_PASSWORD}" --silent >/dev/null 2>&1; do
-    sleep 1
-done
-
-mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<EOF
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
 
 FLUSH PRIVILEGES;
 EOF
 
-wait ${server_pid}
+# Start MariaDB with init file
+exec mysqld_safe --user=mysql --datadir=/var/lib/mysql --init-file=/tmp/init.sql --socket=/run/mysqld/mysqld.sock --bind-address=0.0.0.0
